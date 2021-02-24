66b3dbf73f646ce9c6525de014969ad4fc2fbd04
Ewan Harris <ewanharris93@gmail.com>
2021-02-24 12:01:49 +0000

    feat(updates): add update logic for Titanium and Alloy npm packages
    checkUpdates will check for the appc tooling by default, but when requested will check for the OSS
    tooling. This will allow the editors to check the user settings and supply the right choice

--- src/updates/titanium/index.ts
+++ src/updates/titanium/index.ts
@@ -1,5 +1,7 @@
 import * as sdk from './sdk';
+import * as cli from './cli';
 
 export {
+	cli,
 	sdk
 };
--- src/updates/util.ts
+++ src/updates/util.ts
@@ -1,3 +1,6 @@
+import { run } from 'appcd-subprocess';
+import * as libnpm from 'libnpm';
+
 interface InstallErrorMetadata {
 	code?: number;
 	errorCode?: string;
@@ -19,5 +22,56 @@ export class InstallError extends Error {
 
 		this.code = 'EINSTALLFAILED';
 		this.metadata = metadata || {};
+	}
+}
+
+/**
+ * Check the version of a package that is installed globally via npm
+ *
+ * @param {String} name The package to check for
+ */
+export async function checkInstalledNpmPackageVersion (name: string): Promise<string|undefined> {
+	try {
+		const { stdout } = await run('npm', [ 'ls', `${name}`, '--json', '--depth', '0', '--global' ], { shell: true });
+		const { dependencies } = JSON.parse(stdout);
+		return dependencies[name]?.version;
+	} catch (error) {
+		// squelch
+	}
+}
+
+/**
+ * Check the latest version of a package on the npm registry
+ *
+ * @param {String} name The package to look up
+ */
+export async function checkLatestNpmPackageVersion(name: string): Promise<string> {
+	const { version } = await libnpm.manifest(`${name}@latest`);
+	return version;
+}
+
+/**
+ * Install a package from npm
+ *
+ * @param {String} name The package to install
+ * @param {String} version The version to install
+ */
+export async function installNpmPackage(name: string, version: string): Promise<void> {
+	const { code, stdout, stderr } = await run('npm', [ 'install', '-g', `${name}@${version}`, '--json' ], { shell: true, ignoreExitCode: true });
+	if (code) {
+		const metadata = {
+			errorCode: '',
+			exitCode: code,
+			stderr,
+			stdout,
+			command: `npm install -g ${name}@${version}`
+		};
+		try {
+			const jsonResponse = JSON.parse(stdout);
+			metadata.errorCode = jsonResponse.error && jsonResponse.error.code;
+		} catch (error) {
+			// squash
+		}
+		throw new InstallError('Failed to install package', metadata);
 	}
 }
