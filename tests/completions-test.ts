bda3a02d1dbf0591bb619e9819e526aedbddba6e
Ewan Harris <ewanharris93@gmail.com>
2021-03-04 16:03:06 +0000

    test(completions): update npm mock to reflect code changes

--- tests/completions-test.ts
+++ tests/completions-test.ts
@@ -3,13 +3,15 @@ import { CustomError } from '../src/completions/util';
 
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
+import child_process from 'child_process';
 import fs from 'fs-extra';
 import mockFS from 'mock-fs';
 import os from 'os';
 import * as path from 'path';
+import sinon from 'sinon';
 
 import { parsers } from './fixtures/parsers';
-import globalDirectories from 'global-dirs';
+import { createChildMock } from './util';
 
 chai.use(chaiAsPromised);
 const expect = chai.expect;
@@ -50,7 +52,19 @@ function mockAppcCli (noInstall = false) {
 }
 
 function mockNpmAlloy (noInstall = false) {
-	const installPath = globalDirectories.npm.packages;
+	const installPath = path.join(os.homedir(), 'node_modules');
+	const npmChild = createChildMock();
+	const stub = global.sandbox.stub(child_process, 'spawn');
+
+	stub
+		.withArgs('npm', [ 'root', '-g' ], sinon.match.any)
+		.returns(npmChild);
+
+	setTimeout(() => {
+		npmChild.stdout?.emit('data', installPath);
+		npmChild.emit('close', 0);
+	}, 500);
+
 	if (noInstall) {
 		return {
 			[installPath]: {}
@@ -94,7 +108,7 @@ describe('completions', () => {
 		mockFS.restore();
 	});
 
-	describe('completions.generateAlloyCompletions', () => {
+	describe.only('completions.generateAlloyCompletions', () => {
 		it('Generate Alloy Completions from appc cli', async () => {
 			mockFS(mockAppcCli());
 			const completions = await generateAlloyCompletions(true);
