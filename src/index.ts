05ac53c512edf156a21e13dd5d303c6858bb88fd
Ewan Harris <ewanharris93@gmail.com>
2021-09-01 14:02:28 +0100

    refactor: expose error classes to help typechecking

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import * as completion from './completions';
 import * as environment from './environment';
 import * as updates from './updates';
+import { InstallError } from './util';
+import { CustomError } from './completions/util';
+
+const Errors = {
+	CustomError,
+	InstallError
+};
 
 export {
+	Errors,
 	completion,
 	environment,
 	updates
